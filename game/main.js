2752bc948ed7d54faeb0a4a3fd0eebe63e156855
Ali Umar <ali.umar.work@gmail.com>
2021-11-09 20:50:08 -0500

    removed console

--- game/main.js
+++ game/main.js
@@ -1,9 +1,9 @@
-import { Init, CURRENT_STATE, Update, Draw, Filter, GAME_STATES } from './lifecycle.js';
+import { Init, CURRENT_STATE, Update, Draw, Filter, GAME_STATES, username } from './lifecycle.js';
+import { sendScore } from './network.js';
 
 Init();
 
 const gameLoop = () => {
-    console.log(CURRENT_STATE);
     switch (CURRENT_STATE) {
         case GAME_STATES["INITIAL"]:
             Filter();
