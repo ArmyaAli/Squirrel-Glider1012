bb3d101e93e26da7d5707ddb55fa32ca7ec0152d
Ringo Chui <ringochewyis@gmail.com>
2021-11-16 16:24:21 -0500

    Playable collision detection and added collision for ceiling and floor.

--- game/collision.js
+++ game/collision.js
@@ -1,22 +1,33 @@
-import { TREES, PLAYER, SCORE } from "./entities.js";
+import { TREES, PLAYER, MAX_HEIGHT, MAX_WIDTH } from "./entities.js";
+import { setCurrentState, incrementScore, STATES } from "./lifecycle.js";
 
 export const collisionCheck = () => {
     const trees = TREES.position;
     const player = PLAYER.position;
-    const offset = 0;
-    // console.log("Trees", trees);
-    // get the 6 ranges that our player will not be allowed to touch
+    const offset = 20;
+
     if (trees.length > 0) {
         for (let i = 0; i < trees.length; ++i) {
-            if (player.x > trees[i].xB && player.x < trees[i].xB + TREES.width + offset) {
-                if (player.y > trees[i].yT || player.y < trees[i].yB + TREES.heightBranch - offset) {
-                    console.log("GAMEOVER");
+            if (player.x > trees[i].xT && player.x < trees[i].xT + TREES.width) {
+                if (player.y + PLAYER.height > trees[i].yT + offset || player.y < trees[i].yB + TREES.heightBranch - offset) {
+                    PLAYER.currentState = PLAYER.states["Dead"];
+                    setCurrentState(STATES["GAME_OVER"]);
                 } else {
-
+                    if (Math.floor(player.x) == Math.floor(trees[i].xT + TREES.width) - 1) {
+                        incrementScore();
+                    }
                 }
             }
         }
     }
 
-    // console.log("Player", PLAYER.position);
+    if (PLAYER.y >= (MAX_HEIGHT - 75)) {
+        PLAYER.currentState = PLAYER.states["Dead"];
+        setCurrentState(STATES["GAME_OVER"]);
+    }
+
+    if (PLAYER.y <= 0) {
+        PLAYER.currentState = PLAYER.states["Dead"];
+        setCurrentState(STATES["GAME_OVER"]);
+    }
 }
